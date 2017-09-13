f19616f2adc53c77315f0216a56fc1bc5501d708
filipobsivac <filip.obsivac@centrum.cz>
2017-09-13 17:09:06 +0200

    element parameter classes add

---
+++ thesis/src/thesis/Classes/ElementParameters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace thesis.Classes
+{
+    public class ElementParameters
+    {
+        public string Name { get; set; }
+
+        //key = bond type
+        public Dictionary<int, double> A { get; set; }
+        public Dictionary<int, double> B { get; set; }
+
+        public ElementParameters()
+        {
+            A = new Dictionary<int, double>();
+            B = new Dictionary<int, double>();            
+        }
+    }
+}

bb8a47d1f57632b05d6363448495520514a4bcb8
filipobsivac <filip.obsivac@centrum.cz>
2017-09-15 14:51:38 +0200

    fixed project

---
+++ bc_thesis/bc_thesis/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bc_thesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("thesis");
+            Console.ReadKey();
+        }
+    }
+}
